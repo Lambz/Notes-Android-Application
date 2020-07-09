84c959db8797b081d78401f2afba3f454cbcf8aa
chetu97 <chaitanya.sanoriya@gmail.com>
2020-07-09 13:45:52 -0400

    first commit

---
+++ app/src/main/java/com/lambton/projects/note_wethree_android/activities/NoteDetailActivity.java
@@ -0,0 +1,16 @@
+package com.lambton.projects.note_wethree_android.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lambton.projects.note_wethree_android.R;
+
+public class NoteDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_detail);
+    }
+}
