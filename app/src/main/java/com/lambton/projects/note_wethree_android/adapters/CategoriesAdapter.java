415a44cee9ca8d084fdee8f81c066f414dd56c3d
chetu97 <chaitanya.sanoriya@gmail.com>
2020-07-10 02:45:08 -0400

    Created Categories Recycler View Adapter

---
+++ app/src/main/java/com/lambton/projects/note_wethree_android/adapters/CategoriesAdapter.java
@@ -0,0 +1,63 @@
+package com.lambton.projects.note_wethree_android.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.lambton.projects.note_wethree_android.R;
+
+public class CategoriesAdapter extends RecyclerView.Adapter<CategoriesAdapter.ViewHolder> {
+
+    private Context mContext;
+
+    public CategoriesAdapter(Context context)
+    {
+        this.mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View listItem= layoutInflater.inflate(R.layout.custom_list_item, parent, false);
+        ViewHolder viewHolder = new ViewHolder(listItem);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+//        final MyListData myListData = listdata[position];
+//        holder.textView.setText(listdata[position].getDescription());
+//        holder.imageView.setImageResource(listdata[position].getImgId());
+//        holder.relativeLayout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(view.getContext(),"click on item: "+myListData.getDescription(),Toast.LENGTH_LONG).show();
+//            }
+//        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView mTitleTextView;
+        public TextView mNumNotesTextView;
+        public ConstraintLayout mConstraintLayout;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mTitleTextView = itemView.findViewById(R.id.title_textview);
+            mNumNotesTextView = itemView.findViewById(R.id.num_notes_textview);
+            mConstraintLayout = itemView.findViewById(R.id.item_contraint_layout);
+        }
+    }
+}
