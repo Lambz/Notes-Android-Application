ae38398ef48c881afe7360e591256cc40bfcf50c
Chetan Singh <chetan.singh.0602@gmail.com>
2020-07-10 19:23:21 -0400

    Added database and relationship logic

---
+++ app/src/main/java/com/lambton/projects/note_wethree_android/dataHandler/NoteDatabase.java
@@ -0,0 +1,28 @@
+package com.lambton.projects.note_wethree_android.dataHandler;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Category.class, Note.class}, version = 1)
+public abstract class NoteDatabase extends RoomDatabase {
+
+    private static final String NOTE_DB = "noteDatabase.db";
+    public static volatile NoteDatabase instance;
+
+    static synchronized NoteDatabase getInstance(Context context) {
+        if(instance == null) {
+            instance = createInstance(context);
+        }
+        return instance;
+    }
+
+    private static NoteDatabase createInstance(final Context context) {
+        return Room.databaseBuilder(context, NoteDatabase.class, NOTE_DB).build();
+    }
+
+    public abstract NoteDataInterface getNoteDataInterface();
+    public abstract CategoryDataInterface getCategoryDataInterface();
+}
