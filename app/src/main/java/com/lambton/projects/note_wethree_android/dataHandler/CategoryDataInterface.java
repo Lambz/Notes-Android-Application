fa0a893f1714ca4f021c5ec99a9294064eed11ca
Chetan Singh <chetan.singh.0602@gmail.com>
2020-07-13 21:24:56 -0400

    Updated CategoryDataInterface

--- app/src/main/java/com/lambton/projects/note_wethree_android/dataHandler/CategoryDataInterface.java
+++ app/src/main/java/com/lambton/projects/note_wethree_android/dataHandler/CategoryDataInterface.java
@@ -1,17 +1,37 @@
 package com.lambton.projects.note_wethree_android.dataHandler;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Transaction;
 
 import com.lambton.projects.note_wethree_android.dataHandler.entity.Category;
+import com.lambton.projects.note_wethree_android.dataHandler.entity.Note;
+
+import java.util.List;
 
 @Dao
 public interface CategoryDataInterface {
-
-    @Insert
+    @Transaction
+    @Insert(onConflict = OnConflictStrategy.ABORT)
     void insertCategory(Category category);
+    @Transaction
     @Delete
     void deleteCategory(Category category);
+    @Transaction
+    @Query("SELECT * FROM CATEGORY_DATA WHERE id=:categoryId")
+    LiveData<Category> getCategory(int categoryId);
 
+//    for Note
+    @Transaction
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    void insertNote(Note note);
+
+//    pojo query to fetch data
+    @Transaction
+    @Query("SELECT * FROM category_data")
+    LiveData<List<Category>> loadAllCategories();
 }
