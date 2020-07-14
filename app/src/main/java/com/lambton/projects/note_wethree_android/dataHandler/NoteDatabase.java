0bb236bf2dffa170cb2cc617df56fc81010a8774
Chetan Singh <chetan.singh.0602@gmail.com>
2020-07-14 02:56:18 -0400

    fixed issues with db files

--- app/src/main/java/com/lambton/projects/note_wethree_android/dataHandler/NoteDatabase.java
+++ app/src/main/java/com/lambton/projects/note_wethree_android/dataHandler/NoteDatabase.java
@@ -11,7 +11,7 @@ import com.lambton.projects.note_wethree_android.dataHandler.dao.NoteDataInterfa
 import com.lambton.projects.note_wethree_android.dataHandler.entity.Category;
 import com.lambton.projects.note_wethree_android.dataHandler.entity.Note;
 
-@Database(entities = {Category.class, Note.class}, version = 1)
+@Database(entities = {Category.class, Note.class}, version = 1, exportSchema = false)
 public abstract class NoteDatabase extends RoomDatabase {
 
     private static final String NOTE_DB = "noteDatabase.db";
